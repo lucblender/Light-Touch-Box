e4480a5db0c32577519391996616b0e5947d28f2
Lucas <lucasbonvin@hotmail.com>
2020-06-20 22:23:13 +0200

    Auto stash before rebase of "refs/heads/master"

---
+++ lightTouchBox/credential.h
@@ -0,0 +1,6 @@
+char networkSSID[] = "your ssid";
+char networkPASSWORD[] = "your password";
+
+char mqttSERVER[] = "broker.hivemq.com";
+char mqttUSERNAME[] = "mqtt";
+char mqttPASSWORD[] = "mqtt";
--- lightTouchBox/lightTouchBox.ino
+++ lightTouchBox/lightTouchBox.ino
@@ -1,20 +1,11 @@
 #include <CapacitiveSensor.h>
 #include <Adafruit_NeoPixel.h>
 
+#include <ArduinoBLE.h>
 #include <WiFiNINA.h> 
 #include <PubSubClient.h>
-
-
-
-
-const char* ssid = networkSSID;
-const char* password = networkPASSWORD;
-const char* mqttServer = mqttSERVER;
-const char* mqttUsername = mqttUSERNAME;
-const char* mqttPassword = mqttPASSWORD;
-
-WiFiClient wifiClient;
-PubSubClient client(wifiClient);
+#include "credential.h"
+#include "utility/wifi_drv.h"
 
 #define DEBUG
 
@@ -30,28 +21,65 @@ PubSubClient client(wifiClient);
 
 #define BAUD_RATE         115200
 #define LED_STRIP_PIN     6
-#define THREASHOLD_OVER   250
+#define THREASHOLD_OVER   400
 #define THREASHOLD_TOUCH  2000
 #define LED_COUNT         16
 #define LED_SINUS_LENGHT  12.4
-#define DEBOUNCE_LIMIT    5
+#define DEBOUNCE_LIMIT    10
+#define DELAY_MS          100
+#define BLE_SKIP_MS       15000
+#define BLE_SKIP_CYCLE    (BLE_SKIP_MS/DELAY_MS)
 
+char subTopic[] = "UUIDA/ligthTouch";
+char pubTopic[] = "UUIDB/ligthTouch";
+
+char* ssid = networkSSID;
+char* password = networkPASSWORD;
+const char* mqttServer = mqttSERVER;
+const char* mqttUsername = mqttUSERNAME;
+const char* mqttPassword = mqttPASSWORD;
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+//Bluetooth parameters
+BLEUnsignedIntCharacteristic capacitiveMeasureCharacteristic("2A58", BLERead | BLENotify);
+BLEBooleanCharacteristic configurationDoneCharacteristic("41300001-EB90-09D7-0001-413031360000", BLERead | BLEWrite);
+BLECharacteristic ssidCharacteristic("41300001-EB90-09D7-0001-413031360001", BLERead | BLEWrite,32);
+BLECharacteristic passwordCharacteristic("41300001-EB90-09D7-0001-413031360002", BLERead | BLEWrite,63);
+BLEService lightBoxService("19B10010-E8F2-537E-4F6C-D104768A1214"); 
+//Actual value of bluetooth characteristic
+boolean configurationDoneBle = false;
+char ssidValue[32] ;
+char passwordValue[63] ;
+
+//Wireless connection status
+boolean wifiConnectionSuccess = false;
+boolean bleConnected = false;
+boolean bleConfigured = false;
+
+//cycle in loop since debounce started
 uint8_t debounceCycle = 0;
+//cycle in loop since ble advertising started
+uint8_t bleSkipCycle = 0;
+
+//offset of light incrementing to do light animation
+uint8_t lightOffset = 0;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
-
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
 
-uint8_t b = 0;
-
+//all state machin states
 enum{
+  INIT,
+  BLE_CONFIGURATION,
+  WIFI_CONNECTION,
   IDLE,
   OVERING_DEBOUNCE,
   OVERING,
-  TOUCH_DEBOUNCE,
   TOUCH
-}stateSelf = IDLE;
-int lastStateSelf = IDLE;
+}stateSelf = INIT;
+int lastStateSelf = INIT;
 
 enum{
   _IDLE,
@@ -65,42 +93,185 @@ enum{
 }stateLight = IDLE_LIGHT;
 int lastStateLight = IDLE_LIGHT;
 
+enum{
+  OFF,
+  CONNECTING,
+  FAIL,  
+  ON
+}stateWifi = OFF;
+
 
 void setup_wifi() 
 {
+  int retry = 0;
+  stateWifi = CONNECTING;
   delay(10);
   
   // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  
+  lightConnectionStatus();
+  DEBUG_PRINT("Connecting to ");
+  DEBUG_PRINTLN(ssid);
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) 
+  while (WiFi.status() != WL_CONNECTED && retry < 50) 
   {
-    delay(500);
-    Serial.print(".");
+    lightConnectionStatus();
+    delay(200);
+    DEBUG_PRINT(".");
+    retry ++;
   }
 
-  randomSeed(micros());
+  if(WiFi.status() != WL_CONNECTED){
+    stateWifi = FAIL;
+    wifiConnectionSuccess = false;  
+    DEBUG_PRINTLN("WiFi connection failed!!!");
+    for(int i = 0; i<200;i++)
+    {
+      lightConnectionStatus();
+      delay(5);          
+      strip.clear();
+      strip.show();
+    }    
+    NVIC_SystemReset();
+  }
+  else{
+    stateWifi = ON;
+    wifiConnectionSuccess = true;
+    randomSeed(micros());
+  
+    DEBUG_PRINTLN("WiFi connected");
+    DEBUG_PRINTLN("IP address: ");
+    DEBUG_PRINTLN(WiFi.localIP());
+    for(int i = 0; i<200;i++)
+    {
+      lightConnectionStatus();
+      delay(5);          
+      strip.clear();
+      strip.show();
+    }
+  }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+}
+
+void setup_ble(){
+// begin initialization
+  if (!BLE.begin()) {
+    DEBUG_PRINTLN("starting BLE failed!");
+
+    while (1);
+  }
+  //retreive the first 8 byte of the serial number to add it to the 
+  //bluetooth local name
+  volatile uint32_t val1;
+  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
+  val1 = *ptr1;
+
+  Serial.print("chip id: 0x ");
+  char buf[18];
+  sprintf(buf, "LIGHTBOX-%8x", val1);
+  buf[17] = '\0';
+  Serial.println(buf);
+  
+  // set the local name peripheral advertises
+  BLE.setLocalName(buf);  
+  // set the UUID for the service this peripheral advertises:
+  BLE.setAdvertisedService(lightBoxService);
+  
+  if(bleConfigured == false){  
+    // add the characteristics to the service
+    lightBoxService.addCharacteristic(configurationDoneCharacteristic);
+    lightBoxService.addCharacteristic(capacitiveMeasureCharacteristic);
+    lightBoxService.addCharacteristic(ssidCharacteristic);
+    lightBoxService.addCharacteristic(passwordCharacteristic);
+  
+    configurationDoneCharacteristic.setValue(configurationDoneBle);
+    
+    sprintf(ssidValue,ssid);
+    ssidCharacteristic.setValue((unsigned char *)ssidValue,32) ;
+    
+    sprintf(passwordValue,password);
+    passwordCharacteristic.setValue((unsigned char *)passwordValue,63) ;
+  
+    // add the service
+    BLE.addService(lightBoxService);
+  
+    BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+    BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+  
+    configurationDoneCharacteristic.setEventHandler(BLEWritten, appCharacteristicWrittenConfigurationDone);
+    ssidCharacteristic.setEventHandler(BLEWritten, appCharacteristicWrittenSsid);
+    passwordCharacteristic.setEventHandler(BLEWritten, appCharacteristicWrittenPassword);
+    //bleConfigured = true;
+  }
+
+  // start advertising
+  BLE.advertise();
+}
+
+void appCharacteristicWrittenConfigurationDone(BLEDevice central, BLECharacteristic characteristic) {
+  DEBUG_PRINT("ConfigurationDone Characteristic event, written: ");
+  DEBUG_PRINTLN(characteristic.uuid());
+
+  configurationDoneBle = characteristic.value();
+  DEBUG_PRINTLN(configurationDoneBle);
+}
+  
+void appCharacteristicWrittenSsid(BLEDevice central, BLECharacteristic characteristic) {
+  
+  DEBUG_PRINT(ssid);
+  DEBUG_PRINTLN("TEST");
+  DEBUG_PRINT("SSID Characteristic event, written: ");
+  DEBUG_PRINTLN(characteristic.uuid());
+
+  DEBUG_PRINT("Admin : SSID recu ");
+  DEBUG_PRINTLN(characteristic.valueLength());
+  sprintf(ssidValue,"%32c",NULL);
+  strncpy(ssidValue,(char*)characteristic.value(),characteristic.valueLength());
+  ssidValue[characteristic.valueLength()] = '\0';
+  ssid = ssidValue;
+}
+
+void appCharacteristicWrittenPassword(BLEDevice central, BLECharacteristic characteristic) {
+  DEBUG_PRINT("Password Characteristic event, written: ");
+  DEBUG_PRINTLN(characteristic.uuid());
+
+  DEBUG_PRINT("Admin : Password recu ");
+  DEBUG_PRINTLN(characteristic.valueLength());
+  sprintf(passwordValue,"%63c",NULL);
+  strncpy(passwordValue,(char*)characteristic.value(),characteristic.valueLength());
+  passwordValue[characteristic.valueLength()] = '\0';
+  password = passwordValue;
+  DEBUG_PRINTLN(passwordValue);
+}
+
+void blePeripheralConnectHandler(BLEDevice central) {
+  // central connected event handler
+  DEBUG_PRINT("Connected event, central: ");
+  DEBUG_PRINTLN(central.address());
+  bleConnected = true;
+}
+
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  DEBUG_PRINT("Disconnected event, central: ");
+  DEBUG_PRINTLN(central.address());
+  // start advertising
+  BLE.advertise();
+  bleConnected = false;
 }
 
 void callback(char* topic, byte* payload, unsigned int length) 
 {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  DEBUG_PRINT("Message arrived [");
+  DEBUG_PRINT(topic);
+  DEBUG_PRINT("] ");
   for (int i = 0; i < length; i++) 
   {
-    Serial.print((char)payload[i]);
+    DEBUG_PRINT((char)payload[i]);
   }
-  Serial.println();
+  DEBUG_PRINTLN();
   if ((char)payload[0] == '0') 
   {
     statePartner =   _IDLE;
@@ -120,21 +291,21 @@ void reconnect()
   // Loop until we're reconnected
   while (!client.connected()) 
   {
-    Serial.print("Attempting MQTT connection...");
+    DEBUG_PRINT("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ArduinoClient-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str(), mqttUsername, mqttPassword)) 
     {
-      Serial.println("connected");
+      DEBUG_PRINTLN("connected");
       // ... and resubscribe
       client.subscribe(subTopic);
     } else 
     {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
+      DEBUG_PRINT("failed, rc=");
+      DEBUG_PRINT(client.state());
+      DEBUG_PRINTLN(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -143,29 +314,26 @@ void reconnect()
 
 void setup()                    
 {
-  Serial.begin(BAUD_RATE);
-  
-  setup_wifi();
-  client.setServer(mqttServer, 1883);
-  client.setCallback(callback);
-   
+  Serial.begin(BAUD_RATE);    
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show(); // Initialize all pixels to 'off' 
 }
 
 void loop()                    
 {
+  BLE.poll();
   long total1 =  cs_4_2.capacitiveSensor(80);  
 
-  DEBUG_PRINTLN(total1);
+  capacitiveMeasureCharacteristic.setValue(total1);
 
-
-  if (!client.connected()) 
-  {
-    reconnect();
+  //DEBUG_PRINTLN(total1);
+  if(wifiConnectionSuccess == true){
+    if (!client.connected()) 
+    {
+      reconnect();
+    }
+    client.loop();
   }
-  client.loop();
-
   
   //TEMPORARY will use mqtt to send and retreive the capacitive sensor data
   if (Serial.available() > 0) {
@@ -180,7 +348,24 @@ void loop()
   }
 
   switch (lastStateSelf)
-  {
+  {    
+    case INIT:      
+      stateSelf = BLE_CONFIGURATION;
+      break;
+    case BLE_CONFIGURATION:
+      if(bleConnected == true)
+        bleSkipCycle = 0;
+      else
+        bleSkipCycle++;
+      
+      lightConnectionStatus();
+      if(configurationDoneBle == true || bleSkipCycle>BLE_SKIP_CYCLE)        
+        stateSelf = WIFI_CONNECTION;
+      break;
+    case WIFI_CONNECTION:
+      if(wifiConnectionSuccess == true)              
+        stateSelf = IDLE; 
+      break;
     case IDLE:
       //define in what state we are
       if(total1>THREASHOLD_TOUCH)
@@ -220,6 +405,23 @@ void loop()
     char payLoad[1];
     switch (stateSelf)
     {
+    case INIT:     
+      DEBUG_PRINTLN("Enter INIT"); 
+      break;
+    case BLE_CONFIGURATION:
+      setup_ble();
+      DEBUG_PRINTLN("Enter BLE_CONFIGURATION"); 
+      break;
+    case WIFI_CONNECTION:
+      DEBUG_PRINTLN("Enter WIFI_CONNECTION"); 
+      BLE.stopAdvertise();
+      BLE.end();  
+      wiFiDrv.wifiDriverDeinit();
+      wiFiDrv.wifiDriverInit();
+      setup_wifi();
+      client.setServer(mqttServer, 1883);
+      client.setCallback(callback);
+      break;
       case IDLE:      
         itoa(0, payLoad, 10);
         client.publish(pubTopic, payLoad);
@@ -245,16 +447,14 @@ void loop()
   
   switch (lastStateLight)
   {
-    case IDLE_LIGHT:
-        DEBUG_PRINTLN("IDLE_LIGHT");  
-      if((stateSelf!=IDLE&&stateSelf!=OVERING_DEBOUNCE) || statePartner!=_IDLE){
+    case IDLE_LIGHT: 
+      if((stateSelf==OVERING||stateSelf==TOUCH) || statePartner!=_IDLE){
         stateLight = ON_LIGHT;
       }
       break;
     case ON_LIGHT:
-        DEBUG_PRINTLN("ON_LIGHT");  
       light();
-      if((stateSelf==IDLE||stateSelf==OVERING_DEBOUNCE) && statePartner==_IDLE){
+      if((stateSelf!=OVERING&&stateSelf!=TOUCH) && statePartner==_IDLE){
         stateLight = IDLE_LIGHT;
       }
       break;
@@ -276,9 +476,8 @@ void loop()
         break;
     }
     lastStateLight = stateLight;
-  }
-  
-  delay(100);   
+  }  
+  delay(DELAY_MS);   
 }
 
 void light(){
@@ -288,7 +487,6 @@ void light(){
 
     uint32_t rgbcolorSelf;
     uint8_t offsetSelf;
-
 
     uint32_t rgbcolorPartner;
     uint8_t offsetPartner;
@@ -324,14 +522,58 @@ void light(){
         break;
     }
     
-    int valueSelf = (sin(radians((i+b)*(360/LED_SINUS_LENGHT)))+1)*offsetSelf;          
+    int valueSelf = (sin(radians((i+lightOffset)*(360/LED_SINUS_LENGHT)))+1)*offsetSelf;          
     rgbcolorSelf = strip.ColorHSV(500, 255, valueSelf); 
           
-    int valuePartner = (sin(radians((b)*(360/LED_SINUS_LENGHT)))+1)*offsetPartner;          
+    int valuePartner = (sin(radians((lightOffset)*(360/LED_SINUS_LENGHT)))+1)*offsetPartner;          
     rgbcolorPartner = strip.ColorHSV(43690, 127, valuePartner); 
     //the final color is the addition of self and partner color
-    strip.setPixelColor((i+b)%LED_COUNT, rgbcolorSelf+rgbcolorPartner);
+    strip.setPixelColor((i+lightOffset)%LED_COUNT, rgbcolorSelf+rgbcolorPartner);
   }    
   strip.show();
-  b++%16;
+  lightOffset++%16;
+}
+void lightConnectionStatus(){    
+  //go through all leds to light them properly
+  for(int i= 0; i<LED_COUNT; i++){
+    int hsvValue;
+    uint32_t rgbcolor;
+
+    //create an offset depending of the our capacitive sensor
+    switch (stateSelf)
+    {
+      case BLE_CONFIGURATION:    
+        if(bleConnected == false){                
+          hsvValue = (sin(radians((i+lightOffset)*(360/LED_SINUS_LENGHT)))+1)*10;          
+          rgbcolor = strip.ColorHSV(42962, 255, hsvValue); 
+        }
+        else{
+          hsvValue = (sin(radians((lightOffset)*(360/LED_SINUS_LENGHT)))+1)*10;          
+          rgbcolor = strip.ColorHSV(42962, 255, hsvValue);           
+        }
+        break;
+      case WIFI_CONNECTION:   
+        switch(stateWifi){
+          case OFF:
+          case CONNECTING:
+            hsvValue = (sin(radians((lightOffset)*(360/LED_SINUS_LENGHT)))+1)*10;        
+            rgbcolor = strip.ColorHSV(0, 0, hsvValue); 
+            break;
+          case ON:
+            hsvValue = (sin(radians((lightOffset)*(360/LED_SINUS_LENGHT)))+1)*10;        
+            rgbcolor = strip.ColorHSV(19478, 150, hsvValue); 
+            break;
+          case FAIL:
+            hsvValue = (sin(radians((lightOffset)*(360/LED_SINUS_LENGHT)))+1)*10;        
+            rgbcolor = strip.ColorHSV(5643, 255, hsvValue); 
+            break;
+        }        
+        break;
+    }
+          
+    //the final color is the addition of self and partner color
+    strip.setPixelColor((i+lightOffset)%LED_COUNT, rgbcolor);
+  }    
+  strip.show();
+  lightOffset++%16;
 }
